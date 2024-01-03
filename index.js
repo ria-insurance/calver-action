c6deb2d0c22c22d24c8e13f482043c0f6b031026
Vishnu M Raveendran <vishnu@Vishnus-MacBook-Air.local>
2024-01-03 17:16:46 +0530

    Patch

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ async function createReleaseTag(octokit, owner, repo) {
         if (i != 0){
             version1 = version + '.' + i;
         }
-        release = await getRelease(octokit, owner, repo, version1);
-        console.log(release);
-        if (release.status == 404){
-            return createRelease(octokit, owner, repo);
+        try{
+            release = await getRelease(octokit, owner, repo, version1);
+        } catch(e) {
+            return createRelease(octokit, owner, repo);   
         }
     }
 }
